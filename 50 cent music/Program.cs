6fef274827f70b3c33fcf9b477c1e6b051ab59a9
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-05-19 22:33:52 -0300

    Add project files.

---
+++ 50 cent music/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _50_cent_music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
